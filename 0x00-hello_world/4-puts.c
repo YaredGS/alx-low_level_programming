bd641bab69a7a5bf343f9eb4fe1c3292cf9a54a8
YaredGS <yadabeatz@gmail.com>
2023-03-16 06:04:52 -0700

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+
 /* This Program displays a text uing the FUNCTION, puts.
  * 
- * return 0
- */
-int main()
-{
- puts("\"Programming is like building a multilingual puzzle,\n");
+ * return 0 */
 
-	 return 0;
+int main(void)
+{
+ 	puts("\"Programming is like building a multilingual puzzle,\n");
+
+	 return(0);
 }
