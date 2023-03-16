bac6be2a66fbf605f0b686a99d6ecbfe8aebadae
YaredGS <yadabeatz@gmail.com>
2023-03-16 07:28:28 -0700

    fix4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - main block
- * Return: 0
+ * main - prints to string
+ *
+ * Description: Prints "and that piece of art is useful..." without puts
+ *
+ * Return: 1
  */
 int main(void)
 {
-char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-write(2,  ch, sizeof(ch));
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
 return (1);
 }
