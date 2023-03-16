60b46f8bae226717de5ca8400dfc610092c3dbc8
YaredGS <yadabeatz@gmail.com>
2023-03-16 06:28:04 -0700

    c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
 * main - main block
-* Return - 0
+* Return: 0
 */
 
 int main(void)
 {
-puts("\"Programming is like bulding a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
