686cacef5b5de52078e6fec251c86b884c6667e1
YaredGS <yadabeatz@gmail.com>
2023-04-03 03:28:50 -0700

    Task2

---
+++ 0x07-pointers_arrays_strings/holberton.c
@@ -0,0 +1,17 @@
+# 1 "holberton.h"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "holberton.h"
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
