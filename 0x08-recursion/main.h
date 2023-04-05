e5008975640fd2e6c39f125791950d53ae9465be
YaredGS <yadabeatz@gmail.com>
2023-04-04 23:32:23 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,16 +1,9 @@
-# 1 "holberton.h"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 31 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 32 "<command-line>" 2
-# 1 "holberton.h"
-
+ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -18,3 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif
