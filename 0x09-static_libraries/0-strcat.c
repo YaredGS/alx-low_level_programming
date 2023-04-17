3d1842210bba24ee1dd027104b08ee45657fee4f
YaredGS <yadabeatz@gmail.com>
2023-04-17 02:38:00 -0700

    Pre-Processor

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcat - function that concatenates two strings.
+*
+* @dest: destino
+* @src: source
+*
+* Return: 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	for (a = 0; dest[a] != '\0'; a++)
+	{}
+	while (*(src))
+	{
+		*(dest + a) = *(src);
+		a++;
+		src++;
+	}
+	return (dest);
+}
